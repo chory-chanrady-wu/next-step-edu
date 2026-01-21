d61a3d21e4328695e288f20e9cb819be06571b82
chory-chanrady-wu <chorychanrady.wu@gmail.com>
2026-01-21 21:27:53 +0700

    initailize project

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,6 @@ export default function Home() {
   return (
     <main>
       <h1>Welcome to My Next.js App</h1>
-      <Image
-        src="/images/sample.jpg"
-        alt="Sample Image"
-        width={600}
-        height={400}
-      />
     </main>
   );
 }
