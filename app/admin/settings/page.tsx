88dec47b17e74d28d581b9d58a11a4fae415b37c
chory-chanrady-wu <chorychanrady.wu@gmail.com>
2026-02-25 15:29:15 +0700

    added text in setting page

--- app/admin/settings/page.tsx
+++ app/admin/settings/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const page = () => {
-  return <div></div>;
+  return (
+    <div className="flex items-center justify-center">
+      <h3 className="text-center text-3xl font-semibold text-blue-500 items-center justify-center">
+        Comming Soon ...!
+      </h3>
+    </div>
+  );
 };
 
 export default page;
